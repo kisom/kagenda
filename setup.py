1518d3831b80b42e3af9a592c717d6cb6c2edc0a
Kyle Isom <kyle@imap.cc>
2019-01-11 15:58:42 -0800

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='kagenda',
+    version='1.0',
+    packages=['kagenda'],
+    url='https://github.com/kisom/kagenda',
+    license='MIT',
+    author='kyle',
+    author_email='coder@kyleisom.net',
+    description='Every morning, I like to get my daily kagenda.',
+    scripts=['bin/kagenda']
+)
